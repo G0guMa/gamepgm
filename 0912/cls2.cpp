37dfae6eb4d62a5d558c696cdf7f3fc4588640d4
G0guMa <137869791+G0guMa@users.noreply.github.com>
2024-09-12 11:43:16 +0900

    Add files via upload

---
+++ 0912/cls2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	char string[20];
+	char c;
+	
+	scanf("%s", string);
+	// getchar();
+	scanf("%c", &c);
+	
+	printf("%s\n", string);
+	printf("!!%c!!\n", c);
+	
+	return 0;
+}
+//  출처: https://plustag.tistory.com/1 [자료창고:티스토리]
